10977795f058bd690c529d00460ff486c6d6956e
Robin Berjon <robin@berjon.com>
2013-05-21 18:18:21 +0200

    add notes for development; include node-static

--- bin/bevy.js
+++ bin/bevy.js
@@ -5,6 +5,11 @@ var forever = require("forever-monitor")
 ,   MAX_RESTARTS = 100
 ;
 
+// XXX actually I reckon that this is useless, and should be run with
+// forever directly
+// we just need a small bin script that requires and runs it
+
+// XXX this needs to make sure the configuration is available
 var child = new forever.Monitor(pth.join(__dirname, "../lib/server.js"), {
     max:    MAX_RESTARTS
 });
