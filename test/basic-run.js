0544735a380159c4b77efdcd2edce6eac99f2f2a
Robin Berjon <robin@berjon.com>
2013-05-23 15:56:15 +0200

    testing skeleton

---
+++ test/basic-run.js
@@ -0,0 +1,24 @@
+
+var expect = require("expect.js")
+,   pth = require("path")
+,   fs = require("fs")
+;
+describe("XXX", function () {
+    // XXX
+    //  get a port
+    //  start a server instance using a given configuration
+    //      - store is in tmp, make sure it gets cleaned up
+    //      - use that port
+    //      - use localhost
+    //  deploy
+    //      - a git app
+    //      - a local app
+    //      - a local static (and check that it doesn't copy)
+    //  start
+    //  stop
+    //  remove (all)
+    
+    it("XXX", function () {
+        
+    });
+});
